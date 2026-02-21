a19a148412fc547ecc7c91b3e3aeea38f8e80a95
choiruladamm <choiruladamm@gmail.com>
2026-02-21 23:10:41 +0700

    refactor: separate tanstact-query providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { QueryProvider } from './providers/query-provider';
 
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { refetchOnWindowFocus: false } },
-});
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryProvider>
       <App />
-    </StrictMode>
-  </QueryClientProvider>,
+    </QueryProvider>
+  </StrictMode>,
 );
